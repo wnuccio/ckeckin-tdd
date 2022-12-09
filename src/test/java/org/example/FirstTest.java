b5fc25777cf35149ae942078361950885dbf90b6
wnuccio <wnuccio@hotmail.com>
2022-12-09 12:56:02 +0100

    first commit

---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class FirstTest {
+    @Test
+    void first_test() {
+        assertEquals(1, 1);
+    }
+}
